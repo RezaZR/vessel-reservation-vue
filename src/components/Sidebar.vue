5cb5f4243d5686fbedfe07e231406388a05f2e8f
RezaZR <reza@mahapatih.id>
2019-11-27 18:25:27 +0700

    add sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  .sidebar
+    .container-fluid
+      .row
+        .col-md-12
+          .sidebar__logo 
+            a(href="#") LOGO
+          .sidebar__content
+            .sidebar__content__list.active
+              a(href="#")
+                p Dashboard
+                span
+            .sidebar__content__list
+              a(href="#")
+                p User Management
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p Feeds
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p My Vessels
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p Fleet Request
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p Transactions
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p Coins &#38; Balance
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p Help
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p Settings
+                span
+            .sidebar__content__list 
+              a(href="#")
+                p Log Out
+                span
+</template>
+
+<script>
+export default {
+  name: "sidebar"
+};
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  Sidebar
+</template>
+
+<script>
+import Sidebar from "@/components/Sidebar";
+
+export default {
+  name: "dashboard",
+  components: {
+    Sidebar
+  }
+};
+</script>
